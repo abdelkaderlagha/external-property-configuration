742e19cf76d2f3be32fce83f64cef3455bdb5aad
abdelkaderlagha <abdelkaderlaghaa@outlook.fr>
2020-11-24 11:49:41 +0100

    initial commit

---
+++ src/main/java/gadour/springframework/demo/config/PropertyConfig.java
@@ -0,0 +1,39 @@
+package gadour.springframework.demo.config;
+
+
+import gadour.springframework.demo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${gadour.username}")
+    String username;
+
+    @Value("${gadour.password}")
+    String password;
+
+    @Value("${gadour.dburl}")
+    String dburl;
+
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setDburl(dburl);
+
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties(){
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
---
+++ src/main/java/gadour/springframework/demo/examplebeans/FakeDataSource.java
@@ -0,0 +1,32 @@
+package gadour.springframework.demo.examplebeans;
+
+public class FakeDataSource {
+
+    private String username;
+    private String password;
+    private String dburl;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDburl() {
+        return dburl;
+    }
+
+    public void setDburl(String dburl) {
+        this.dburl = dburl;
+    }
+}
